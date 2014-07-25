c9b6f4cb444c0c5d568e4b3dffa80130bbcd0588
Brandon Weiss <brandon@anti-pattern.com>
2014-07-25 14:22:14 -0700

    Converting tabs to spaces
    That’s what I get for copying and pasting one line of code.

--- ItemLevelOverlay.lua
+++ ItemLevelOverlay.lua
@@ -1,79 +1,79 @@
 local ItemUpgradeInfo = LibStub('LibItemUpgradeInfo-1.0')
 
 local inventorySlotNames = {
-	"HeadSlot",
-	"NeckSlot",
-	"ShoulderSlot",
-	"BackSlot",
-	"ChestSlot",
-	"WristSlot",
-	"HandsSlot",
-	"WaistSlot",
-	"LegsSlot",
-	"FeetSlot",
-	"Finger0Slot",
-	"Finger1Slot",
-	"Trinket0Slot",
-	"Trinket1Slot",
-	"MainHandSlot",
-	"SecondaryHandSlot"
+  "HeadSlot",
+  "NeckSlot",
+  "ShoulderSlot",
+  "BackSlot",
+  "ChestSlot",
+  "WristSlot",
+  "HandsSlot",
+  "WaistSlot",
+  "LegsSlot",
+  "FeetSlot",
+  "Finger0Slot",
+  "Finger1Slot",
+  "Trinket0Slot",
+  "Trinket1Slot",
+  "MainHandSlot",
+  "SecondaryHandSlot"
 }
 
 local inventoryItemOverlayCache = {}
 
 local function getInventoryItemLevelFromInventorySlotName(inventorySlotName)
-	local inventorySlotNumber = select(1, GetInventorySlotInfo(inventorySlotName))
-	local inventoryItemLink   = GetInventoryItemLink("player", inventorySlotNumber)
-	return ItemUpgradeInfo:GetUpgradedItemLevel(inventoryItemLink)
+  local inventorySlotNumber = select(1, GetInventorySlotInfo(inventorySlotName))
+  local inventoryItemLink   = GetInventoryItemLink("player", inventorySlotNumber)
+  return ItemUpgradeInfo:GetUpgradedItemLevel(inventoryItemLink)
 end
 
 local function updateItemOverlay(overlayFrame, inventorySlotName, itemLevel)
-	local cachedInventoryItemOverlay = inventoryItemOverlayCache[inventorySlotName]
+  local cachedInventoryItemOverlay = inventoryItemOverlayCache[inventorySlotName]
 
-	if cachedInventoryItemOverlay then
-		cachedInventoryItemOverlay:SetText(itemLevel)
-	else
-		local text = overlayFrame:CreateFontString(nil, "OVERLAY", "NumberFontNormal")
-		text:SetPoint("TOPLEFT", "Character" .. inventorySlotName, 3, -3)
-		text:Hide()
-		text:SetText(itemLevel)
-		text:Show()
-		inventoryItemOverlayCache[inventorySlotName] = text
-	end
+  if cachedInventoryItemOverlay then
+    cachedInventoryItemOverlay:SetText(itemLevel)
+  else
+    local text = overlayFrame:CreateFontString(nil, "OVERLAY", "NumberFontNormal")
+    text:SetPoint("TOPLEFT", "Character" .. inventorySlotName, 3, -3)
+    text:Hide()
+    text:SetText(itemLevel)
+    text:Show()
+    inventoryItemOverlayCache[inventorySlotName] = text
+  end
 end
 
 local function updateItemOverlays(overlayFrame)
-	for index, inventorySlotName in ipairs(inventorySlotNames) do
-		local itemLevel = getInventoryItemLevelFromInventorySlotName(inventorySlotName)
+  for index, inventorySlotName in ipairs(inventorySlotNames) do
+    local itemLevel = getInventoryItemLevelFromInventorySlotName(inventorySlotName)
 
-		if itemLevel then
-			updateItemOverlay(overlayFrame, inventorySlotName, itemLevel)
-		end
-	end
+    if itemLevel then
+      updateItemOverlay(overlayFrame, inventorySlotName, itemLevel)
+    end
+  end
 end
 
 local function createOverlayFrame()
-	local overlayFrame = CreateFrame("Frame", nil, PaperDollItemsFrame)
+  local overlayFrame = CreateFrame("Frame", nil, PaperDollItemsFrame)
 
-	overlayFrame:SetScript("OnShow", function(self)
-		updateItemOverlays(self)
-	end)
+  overlayFrame:SetScript("OnShow", function(self)
+    updateItemOverlays(self)
+  end)
 
-	overlayFrame:SetScript("OnEvent", function(self, event, ...)
-		if (event == "PLAYER_EQUIPMENT_CHANGED") then
-			updateItemOverlays(self)
-		end
-	end)
+  overlayFrame:SetScript("OnEvent", function(self, event, ...)
+    if (event == "PLAYER_EQUIPMENT_CHANGED") then
+      updateItemOverlays(self)
+    end
+  end)
 
-	overlayFrame:RegisterEvent("PLAYER_EQUIPMENT_CHANGED")
-	overlayFrame:Show()
+  overlayFrame:RegisterEvent("PLAYER_EQUIPMENT_CHANGED")
+  overlayFrame:Show()
 
-	return overlayFrame
+  return overlayFrame
 end
 
 local function initialize()
-	local overlayFrame = createOverlayFrame()
-	updateItemOverlays(overlayFrame)
+  local overlayFrame = createOverlayFrame()
+  updateItemOverlays(overlayFrame)
 end
 
 initialize()
